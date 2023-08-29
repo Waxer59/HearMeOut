6aa07f30ae345ce6a17e3457d3d925b4f97420d1
Waxer59 <hnboyano@gmail.com>
2023-08-29 12:21:20 +0200

    add prisma

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { UsersModule } from './users/users.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, MessagesWsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
